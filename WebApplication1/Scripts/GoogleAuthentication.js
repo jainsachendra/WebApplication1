c270f60a9d5edc8ee29baa9eeeeeca04ad1baa4f
sachendra jain <jainsachendra98@gmail.com>
2020-10-09 14:03:23 +0530

    //

---
+++ WebApplication1/Scripts/GoogleAuthentication.js
@@ -0,0 +1,46 @@
+﻿/// <reference path="jquery-3.4.1.min.js" />
+
+function getAccessToken() {
+    if (location.hash) {
+        if (location.hash.split('access_token=')) {
+            var accessToken = location.hash.split('access_token=')[1].split('&')[0];
+            if (accessToken) {
+                isUserRegistered(accessToken);
+            }
+        }
+    }
+}
+function isUserRegistered(accessToken) {
+    $.ajax({
+        url: '/api/Account/UserInfo',
+        method: 'GET',
+        headers: {
+            'content-type': 'application/JSON',
+            'Authorization': 'Bearer ' + accessToken
+        },
+        success: function (response) {
+            if (response.HasRegistered) {
+                localStorage.setItem('accessToken', accessToken);
+                localStorage.setItem('userName', response.Email);
+                window.location.href = "Data.html";
+            }
+            else {
+                signupExternalUser(accessToken);
+            }
+        }
+    });
+}
+function signupExternalUser(accessToken) {
+    $.ajax({
+        url: '/api/Account/RegisterExternal',
+        method: 'POST',
+        headers: {
+            'content-type': 'application/json',
+            'Authorization': 'Bearer ' + accessToken
+        },
+        success: function (data) {
+            window.location.href = "/api/Account/ExternalLogin?provider=Google&response_type=token&client_id=self&redirect_uri=https%3A%2F%2Flocalhost%3A44372%2Flogin.html&state=4b_Dl0wjik5acX5Ea3ynrfN-PJGMedRM22l4AIUT9uo1";
+        }
+    });
+
+}
